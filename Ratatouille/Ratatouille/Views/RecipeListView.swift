8d7ecbc0870cebdca30eb14dc295f86bf757b34f
Victor Falck-Næss <43470721+Swarmino@users.noreply.github.com>
2023-11-14 12:48:04 +0100

    view setup

---
+++ Ratatouille/Ratatouille/Views/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  Ratatouille
+//
+//  Created by Victor Falck-Næss on 14/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
---
+++ Ratatouille/Ratatouille/Views/SplashView.swift
@@ -0,0 +1,54 @@
+//
+//  SplashView.swift
+//  Ratatouille
+//
+//  Created by Victor Falck-Næss on 14/11/2023.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isFadedOut = false
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // Fading View
+                VStack {
+                    Text("Fading View")
+                }
+                .opacity(isFadedOut ? 0 : 1)
+                .onAppear {
+                    // Use DispatchQueue to delay the animation by 3 seconds
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            self.isFadedOut = true
+                        }
+                    }
+                }
+
+                // New View (Rendered after fade-out)
+                NavigationLink(
+                    destination: NewView(),
+                    isActive: $isFadedOut,
+                    label: {
+                        EmptyView()
+                    }
+                )
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct NewView: View {
+    var body: some View {
+        VStack {
+            Text("New View")
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
